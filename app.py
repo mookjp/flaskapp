9e12390dd7aecf8bfa46a94a015b303c978971f6
Mook <mookjpy@gmail.com>
2014-09-11 01:05:39 +0900

    add another message

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '<h1>Hello World!</h1>'
+    return '<h1>Hello World!</h1> \
+            <p>This is a sample wep application!</p>'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
