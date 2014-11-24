93c38016b0e6495fded05c81fc7340c6e5853321
Mook <mookjpy@gmail.com>
2014-11-24 14:09:14 +0900

    Add feature-a version

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 def hello_world():
     return '<h1>Hello World!</h1> \
             <p>This is a sample wep application!</p>\
-            <strong>This is `development` version.</strong>'
+            <strong>This is `feature-a` version.</strong>'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
