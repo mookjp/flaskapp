ad349c3ab815c5dabd5eb61386078ce72d21ba53
Mook <mookjpy@gmail.com>
2014-12-15 20:34:52 +0100

    Add issue 100

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 def hello_world():
     return '<h1>Hello World!</h1> \
             <p>This is a sample wep application!</p>\
-            <strong>Can you see some versions of this application?</strong>'
+            <strong>issue 100!</strong>'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
